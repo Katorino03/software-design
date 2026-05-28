b7fc549833214a200b241d85b0021d53acf7d90f
Katorino03 <kathleenmaefloresb@gmail.com>
2026-05-28 20:57:58 +0800

    Laboratory Exercise 8

--- lab8/Form1.Designer.cs
+++ lab8/Form1.Designer.cs
@@ -1,39 +1,196 @@
-﻿namespace lab8
+﻿using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace lab8
 {
-	partial class Form1
-	{
-		/// <summary>
-		///  Required designer variable.
-		/// </summary>
-		private System.ComponentModel.IContainer components = null;
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
-		///  Clean up any resources being used.
-		/// </summary>
-		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && (components != null))
-			{
-				components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtISBN = new TextBox();
+            label1 = new Label();
+            btnFetchBook = new Button();
+            txtBookDetails = new TextBox();
+            btnSearchAuthor = new Button();
+            txtAuthorSearch = new TextBox();
+            listBoxBooks = new ListBox();
+            btnPrevApiPage = new Button();
+            btnNextApiPage = new Button();
+            lblPageNumber = new Label();
+            SuspendLayout();
+            // 
+            // txtISBN
+            // 
+            txtISBN.Anchor = AnchorStyles.None;
+            txtISBN.Location = new Point(49, 41);
+            txtISBN.Margin = new Padding(3, 4, 3, 4);
+            txtISBN.Name = "txtISBN";
+            txtISBN.Size = new Size(181, 27);
+            txtISBN.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.None;
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(49, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 20);
+            label1.TabIndex = 1;
+            label1.Text = "ISBN:";
+            label1.Click += label1_Click;
+            // 
+            // btnFetchBook
+            // 
+            btnFetchBook.Anchor = AnchorStyles.None;
+            btnFetchBook.BackColor = Color.RosyBrown;
+            btnFetchBook.Location = new Point(237, 41);
+            btnFetchBook.Margin = new Padding(3, 4, 3, 4);
+            btnFetchBook.Name = "btnFetchBook";
+            btnFetchBook.Size = new Size(109, 31);
+            btnFetchBook.TabIndex = 2;
+            btnFetchBook.Text = "Fetch Book";
+            btnFetchBook.UseVisualStyleBackColor = false;
+            btnFetchBook.Click += btnFetchBook_Click;
+            // 
+            // txtBookDetails
+            // 
+            txtBookDetails.Anchor = AnchorStyles.None;
+            txtBookDetails.Location = new Point(49, 80);
+            txtBookDetails.Margin = new Padding(3, 4, 3, 4);
+            txtBookDetails.Multiline = true;
+            txtBookDetails.Name = "txtBookDetails";
+            txtBookDetails.Size = new Size(297, 265);
+            txtBookDetails.TabIndex = 3;
+            // 
+            // btnSearchAuthor
+            // 
+            btnSearchAuthor.Anchor = AnchorStyles.None;
+            btnSearchAuthor.BackColor = Color.RosyBrown;
+            btnSearchAuthor.Location = new Point(238, 357);
+            btnSearchAuthor.Margin = new Padding(3, 4, 3, 4);
+            btnSearchAuthor.Name = "btnSearchAuthor";
+            btnSearchAuthor.Size = new Size(109, 31);
+            btnSearchAuthor.TabIndex = 5;
+            btnSearchAuthor.Text = "Search Author";
+            btnSearchAuthor.UseVisualStyleBackColor = false;
+            btnSearchAuthor.Click += btnSearchAuthor_Click;
+            // 
+            // txtAuthorSearch
+            // 
+            txtAuthorSearch.Anchor = AnchorStyles.None;
+            txtAuthorSearch.Location = new Point(49, 357);
+            txtAuthorSearch.Margin = new Padding(3, 4, 3, 4);
+            txtAuthorSearch.Name = "txtAuthorSearch";
+            txtAuthorSearch.Size = new Size(181, 27);
+            txtAuthorSearch.TabIndex = 4;
+            // 
+            // listBoxBooks
+            // 
+            listBoxBooks.Anchor = AnchorStyles.None;
+            listBoxBooks.FormattingEnabled = true;
+            listBoxBooks.Location = new Point(49, 402);
+            listBoxBooks.Margin = new Padding(3, 4, 3, 4);
+            listBoxBooks.Name = "listBoxBooks";
+            listBoxBooks.Size = new Size(297, 264);
+            listBoxBooks.TabIndex = 6;
+            // 
+            // btnPrevApiPage
+            // 
+            btnPrevApiPage.Anchor = AnchorStyles.None;
+            btnPrevApiPage.BackColor = Color.RosyBrown;
+            btnPrevApiPage.Location = new Point(49, 676);
+            btnPrevApiPage.Margin = new Padding(3, 4, 3, 4);
+            btnPrevApiPage.Name = "btnPrevApiPage";
+            btnPrevApiPage.Size = new Size(109, 31);
+            btnPrevApiPage.TabIndex = 7;
+            btnPrevApiPage.Text = "Previous Page";
+            btnPrevApiPage.UseVisualStyleBackColor = false;
+            btnPrevApiPage.Click += btnPrevApiPage_Click;
+            // 
+            // btnNextApiPage
+            // 
+            btnNextApiPage.Anchor = AnchorStyles.None;
+            btnNextApiPage.BackColor = Color.RosyBrown;
+            btnNextApiPage.Location = new Point(238, 676);
+            btnNextApiPage.Margin = new Padding(3, 4, 3, 4);
+            btnNextApiPage.Name = "btnNextApiPage";
+            btnNextApiPage.Size = new Size(109, 31);
+            btnNextApiPage.TabIndex = 8;
+            btnNextApiPage.Text = "Next Page";
+            btnNextApiPage.UseVisualStyleBackColor = false;
+            btnNextApiPage.Click += btnNextApiPage_Click;
+            // 
+            // lblPageNumber
+            // 
+            lblPageNumber.Anchor = AnchorStyles.None;
+            lblPageNumber.AutoSize = true;
+            lblPageNumber.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPageNumber.Location = new Point(171, 681);
+            lblPageNumber.Name = "lblPageNumber";
+            lblPageNumber.Size = new Size(53, 20);
+            lblPageNumber.TabIndex = 9;
+            lblPageNumber.Text = "Page 1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Snow;
+            ClientSize = new Size(409, 726);
+            Controls.Add(lblPageNumber);
+            Controls.Add(btnNextApiPage);
+            Controls.Add(btnPrevApiPage);
+            Controls.Add(listBoxBooks);
+            Controls.Add(btnSearchAuthor);
+            Controls.Add(txtAuthorSearch);
+            Controls.Add(txtBookDetails);
+            Controls.Add(btnFetchBook);
+            Controls.Add(label1);
+            Controls.Add(txtISBN);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private TextBox txtISBN;
+        private Label label1;
+        private Button btnFetchBook;
+        private TextBox txtBookDetails;
+        private Button btnSearchAuthor;
+        private TextBox txtAuthorSearch;
+        private ListBox listBoxBooks;
+        private Button btnPrevApiPage;
+        private Button btnNextApiPage;
+        private Label lblPageNumber;
+    }
 }
--- lab8/Form1.cs
+++ lab8/Form1.cs
@@ -1,10 +1,163 @@
+using Newtonsoft.Json.Linq;
+using System.Runtime.InteropServices.JavaScript;
+
 namespace lab8
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Asynchronous method to fetch book details from Google Books API
+        public async Task<string> GetBookInfoFromAPIAsync(string isbn)
+        {
+            string apiUrl = $"https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}&key=AIzaSyCbLIUZCGEHOlCrJo8WHzm5hAdnFryMwZM";
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    var response = await client.GetStringAsync(apiUrl); // Fetch the API response
+                    var jsonResponse = JObject.Parse(response);     // Parse the response as JSON
+
+                    // Extract the book title and authors from the API response
+                    var title = jsonResponse["items"][0]["volumeInfo"]["title"].ToString();
+                    var authors = string.Join(", ", jsonResponse["items"][0]["volumeInfo"]["authors"].Select(a => a.ToString()));
+
+                    return $"Title: {title} \n Authors: {authors}";
+                }
+            }
+            catch (HttpRequestException)
+            {
+                return "Error: Unable to fetch data from the API. Please check your connection or try again later.";
+            }
+            catch (Exception ex)
+            {
+                return $"Error: {ex.Message}";
+            }
+        }
+
+        // Button click event to fetch book details from the API
+        private async void btnFetchBook_Click(object sender, EventArgs e)
+        {
+            var isbn = txtISBN.Text;
+            var bookInfo = await GetBookInfoFromAPIAsync(isbn);
+
+            txtBookDetails.Text = bookInfo;
+        }
+
+        // Variables specifically for API pagination
+        private int apiStartIndex = 0;
+        private const int apiMaxResults = 10;
+        private string currentApiAuthor = "";
+        private string apiKey = "AIzaSyCbLIUZCGEHOlCrJo8WHzm5hAdnFryMwZM";
+
+        // Asynchronous method to search books by author using Google Books API with Pagination
+        public async Task<List<string>> SearchBooksByAuthorAsync(string author, int startIndex)
+        {
+            var bookList = new List<string>();
+
+            // Construct the URL using 'inauthor:', startIndex, maxResults, and your API Key
+            string apiUrl = $"https://www.googleapis.com/books/v1/volumes?q=inauthor:{author}&startIndex={startIndex}&maxResults={apiMaxResults}&key={apiKey}";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Add("User-Agent", "Lab8BookstoreApp/1.0");
+                    var response = await client.GetStringAsync(apiUrl);
+                    var jsonResponse = JObject.Parse(response);
+
+                    // Google returns an "items" array. We must check if it exists in case the search is empty.
+                    if (jsonResponse["items"] != null)
+                    {
+                        foreach (var item in jsonResponse["items"])
+                        {
+                            var volumeInfo = item["volumeInfo"];
+
+                            // Safely extract the title
+                            string title = volumeInfo["title"]?.ToString() ?? "Unknown Title";
+
+                            // Safely extract authors (some books might not list an author)
+                            string authors = "Unknown Author";
+                            if (volumeInfo["authors"] != null)
+                            {
+                                authors = string.Join(", ", volumeInfo["authors"].Select(a => a.ToString()));
+                            }
+
+                            bookList.Add($"{title} by {authors}");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"API Error: {ex.Message}");
+            }
+
+            return bookList;
+        }
+
+        // Helper method to handle updating the UI for the API
+        private async Task LoadApiBooksAsync()
+        {
+            var books = await SearchBooksByAuthorAsync(currentApiAuthor, apiStartIndex);
+            listBoxBooks.DataSource = books;
+
+            if (books.Count == 0 && apiStartIndex > 0)
+            {
+                MessageBox.Show("No more books found for this author.");
+            }
+
+            // Calculate the current page number using math and update the label
+            int currentPage = (apiStartIndex / apiMaxResults) + 1;
+            lblPageNumber.Text = $"Page {currentPage}";
+        }
+
+        // Button click event to start the initial search
+        private async void btnSearchAuthor_Click(object sender, EventArgs e)
+        {
+            currentApiAuthor = txtAuthorSearch.Text;
+
+            if (!string.IsNullOrWhiteSpace(currentApiAuthor))
+            {
+                apiStartIndex = 0;
+                await LoadApiBooksAsync();
+            }
+            else
+            {
+                MessageBox.Show("Please enter an author name.");
+            }
+        }
+
+        // Button click event to load the Next Page of API results
+        private async void btnNextApiPage_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(currentApiAuthor))
+            {
+                apiStartIndex += apiMaxResults;
+                await LoadApiBooksAsync();
+            }
+        }
+
+        // Button click event to load the Previous Page of API results
+        private async void btnPrevApiPage_Click(object sender, EventArgs e)
+        {
+            if (apiStartIndex >= apiMaxResults)
+            {
+                apiStartIndex -= apiMaxResults;
+                await LoadApiBooksAsync();
+            }
+            else
+            {
+                MessageBox.Show("You are already on the first page of the API results.");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
